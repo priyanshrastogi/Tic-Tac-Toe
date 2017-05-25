ecb09fa6e33def52ee9af79d85f75dbd227cff67
Priyansh Rastogi <priyanshrastogi54@gmail.com>
2017-05-25 11:44:27 +0530

    Major Bug fixed

--- javascript.js
+++ javascript.js
@@ -2,10 +2,9 @@ var play = document.querySelector('#play-button');
 
 var squares = document.querySelectorAll("td");
 
-var win_array = [7, 56, 448, 73, 146, 292, 273, 84]
-
 var game_array_X = [];
 var game_array_O = [];
+var turn = 1;
 
 function clearBoard() {
     for(var i=0; i<squares.length; i++) {
@@ -13,42 +12,64 @@ function clearBoard() {
     }
     game_array_X = [];
     game_array_O = [];
+    turn=1;
 }
 
 play.addEventListener('click',clearBoard);
 
-
 function checkWinnerX() {
-    var sum = 0;
-    for(var i in game_array_X)
-        sum += parseInt(game_array_X[i]);
-    //console.log(sum);
-    if(win_array.indexOf(sum)!==-1)
+    if(game_array_X.indexOf(0)!=-1 && game_array_X.indexOf(4)!=-1 && game_array_X.indexOf(8)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(2)!=-1 && game_array_X.indexOf(4)!=-1 && game_array_X.indexOf(6)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(0)!=-1 && game_array_X.indexOf(1)!=-1 && game_array_X.indexOf(2)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(3)!=-1 && game_array_X.indexOf(4)!=-1 && game_array_X.indexOf(5)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(6)!=-1 && game_array_X.indexOf(7)!=-1 && game_array_X.indexOf(8)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(0)!=-1 && game_array_X.indexOf(3)!=-1 && game_array_X.indexOf(6)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(1)!=-1 && game_array_X.indexOf(4)!=-1 && game_array_X.indexOf(7)!=-1)
+        alert("Player X wins!")
+    else if(game_array_X.indexOf(2)!=-1 && game_array_X.indexOf(5)!=-1 && game_array_X.indexOf(8)!=-1)
         alert("Player X wins!")
 }
 
 function checkWinnerO() {
-    var sum = 0;
-    for(var i in game_array_O)
-        sum += parseInt(game_array_O[i]);
-    //console.log(sum);
-    if(win_array.indexOf(sum)!==-1)
+    if(game_array_O.indexOf(0)!=-1 && game_array_O.indexOf(4)!=-1 && game_array_O.indexOf(8)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(2)!=-1 && game_array_O.indexOf(4)!=-1 && game_array_O.indexOf(6)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(0)!=-1 && game_array_O.indexOf(1)!=-1 && game_array_O.indexOf(2)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(3)!=-1 && game_array_O.indexOf(4)!=-1 && game_array_O.indexOf(5)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(6)!=-1 && game_array_O.indexOf(7)!=-1 && game_array_O.indexOf(8)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(0)!=-1 && game_array_O.indexOf(3)!=-1 && game_array_O.indexOf(6)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(1)!=-1 && game_array_O.indexOf(4)!=-1 && game_array_O.indexOf(7)!=-1)
+        alert("Player O wins!")
+    else if(game_array_O.indexOf(2)!=-1 && game_array_O.indexOf(5)!=-1 && game_array_O.indexOf(8)!=-1)
         alert("Player O wins!")
 }
 
-function markX() {
-    this.textContent = 'X';
-    game_array_X.push(this.id);
-    checkWinnerX();
-}
-
-function markO() {
-    this.textContent = 'O';
-    game_array_O.push(this.id);
-    checkWinnerO();
+function changeTurn() {
+    if(turn===1) {
+        turn=2;
+        this.textContent = 'X';
+        game_array_X.push(parseInt(this.id));
+        checkWinnerX();
+    }
+    else if(turn===2) {
+        turn=1;
+        this.textContent = 'O';
+        game_array_O.push(parseInt(this.id));
+        checkWinnerO();
+    }
 }
 
 for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', markX);
-    squares[i].addEventListener('dblclick',markO);
+    squares[i].addEventListener('click', changeTurn);
 }
